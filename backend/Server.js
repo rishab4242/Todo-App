afd2ff66962e530f8c30b3d47f886979e002e131
Rishab <rishabdakhale17@gmail.com>
2025-04-16 22:31:53 +0530

    cloudsetup

--- backend/Server.js
+++ backend/Server.js
@@ -1,15 +1,13 @@
 if (process.env.NODE_ENV != "productions") {
-  require('dotenv').config()
-};
+  require("dotenv").config();
+}
 const express = require("express");
 const mongoose = require("mongoose");
 
 const cors = require("cors");
 const routes = require("./routes/todoRoutes");
 
-const path = require('path');
-
-
+const path = require("path");
 
 
 const app = express();
@@ -23,10 +21,10 @@ app.use(cors());
 app.use("/api", routes);
 
 // Serve static files from React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 mongoose
