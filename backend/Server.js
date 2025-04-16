0825b73c65a02914cf247c54479daf64dc090ca3
Rishab <rishabdakhale17@gmail.com>
2025-04-16 12:28:05 +0530

    deploy stage

--- backend/Server.js
+++ backend/Server.js
@@ -5,6 +5,11 @@ require("dotenv").config();
 const cors = require("cors");
 const routes = require("./routes/todoRoutes");
 
+const path = require('path');
+
+
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", routes);
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
