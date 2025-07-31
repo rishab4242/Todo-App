ced3cac4e939da8940df1a4c4d51903a23305d7d
Rishab <rishabdakhale17@gmail.com>
2025-07-31 23:46:38 +0530

    add signup/login page

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 ये बहुत जरूरी है अगर आपने custom base path यूज़ किया हो तो '/' ही रखें
+  base: '/',
   build: {
     rollupOptions: {
-      input: '/index.html',
+      input: 'index.html',
     },
   },
 })
