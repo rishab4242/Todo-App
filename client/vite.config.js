555651b660a8dbe0a7b87d8e19309034cdcfe75c
Rishab <rishabdakhale17@gmail.com>
2025-08-01 14:27:52 +0530

    update vite.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ very important for Render
+  base: './', // ✅ very important for Render
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
